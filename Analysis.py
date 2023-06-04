77724112df11cabefa190393b830f7cbae725f20
Rchmt-cmd <rachmath.059@gmail.com>
2023-06-04 20:05:50 +0800

    requirements revision

--- Analysis.py
+++ Analysis.py
@@ -1,78 +1,91 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from keras.applications.mobilenet_v2 import preprocess_input
+# import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from keras.applications.mobilenet_v2 import preprocess_input
+# from google.cloud import storage
 
 
+# # TODO upload image result to cloud storage
 
-def get_bounding_box(image, threshold):
-    # Placeholder implementation
-    # Replace this with your actual bounding box calculation logic
-    height, width, _ = image.shape
-    xmin = int(width * 0.25)
-    ymin = int(height * 0.25)
-    xmax = int(width * 0.75)
-    ymax = int(height * 0.75)
-    return xmin, ymin, xmax, ymax
+# storage_client = storage.Client()
 
-def detect_acne(image_path, model, threshold):
-    # Read the input image using OpenCV
-    image = cv2.imread(image_path)
+# def get_bounding_box(image, threshold):
+#     # Placeholder implementation
+#     # Replace this with your actual bounding box calculation logic
+#     height, width, _ = image.shape
+#     xmin = int(width * 0.25)
+#     ymin = int(height * 0.25)
+#     xmax = int(width * 0.75)
+#     ymax = int(height * 0.75)
+#     return xmin, ymin, xmax, ymax
 
-    # Resize the image to the required dimensions
-    if image is None:
-      return ('Wrong path:', image_path)
-    else:
-      image = cv2.resize(image, dsize=(224,224))
+# def detect_acne(data, model, threshold):
+#     file_name = data['name']
+#     bucket_name = data['bucket']
+
+#     image_path = f"gs://{bucket_name}/{file_name}"
+
+#     # Read the input image using OpenCV
+#     image = cv2.imread(image_path)
+
+#     # Resize the image to the required dimensions
+#     if image is None:
+#       return ('Wrong path:', image_path)
+#     else:
+#       image = cv2.resize(image, dsize=(224,224))
 
 
-    # Preprocess the image for model prediction
-    input_data = preprocess_input(np.expand_dims(image, axis=0))
+#     # Preprocess the image for model prediction
+#     input_data = preprocess_input(np.expand_dims(image, axis=0))
 
-    # Perform the prediction using the loaded model
-    predictions = model.predict(input_data)
+#     # Perform the prediction using the loaded model
+#     predictions = model.predict(input_data)
 
-    # Extract the acne instances and their class labels
-    detections = []
-    acne_labels = {
-        0: 'papules',
-        1: 'nodules',
-        2: 'pustules',
-        3: 'comedones'
-    }
+#     # Extract the acne instances and their class labels
+#     detections = []
+#     acne_labels = {
+#         0: 'papules',
+#         1: 'nodules',
+#         2: 'pustules',
+#         3: 'comedones'
+#     }
 
-    for i, prediction in enumerate(predictions[0]):
-        if prediction > threshold:
-            acne_class = acne_labels[i]
-            confidence = float(prediction)
+#     for i, prediction in enumerate(predictions[0]):
+#         if prediction > threshold:
+#             acne_class = acne_labels[i]
+#             confidence = float(prediction)
 
-            detections.append({
-                'class': acne_class,
-                'confidence': confidence
-            })
+#             detections.append({
+#                 'class': acne_class,
+#                 'confidence': confidence
+#             })
 
-    # Draw bounding boxes on the image for detected acne instances
-    if len(detections) > 0:
-        for detection in detections:
-            acne_class = detection['class']
-            confidence = detection['confidence']
+#     # Draw bounding boxes on the image for detected acne instances
+#     if len(detections) > 0:
+#         for detection in detections:
+#             acne_class = detection['class']
+#             confidence = detection['confidence']
 
-            # Get the predicted bounding box coordinates
-            xmin, ymin, xmax, ymax = get_bounding_box(image, threshold)
+#             # Get the predicted bounding box coordinates
+#             xmin, ymin, xmax, ymax = get_bounding_box(image, threshold)
 
-            # Draw the bounding box on the image with purple-red color
-            color = (203, 0, 203)  # Purple-red color (BGR format)
-            cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+#             # Draw the bounding box on the image with purple-red color
+#             color = (203, 0, 203)  # Purple-red color (BGR format)
+#             cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
 
-        # Display the output image with bounding boxes
-        image_result = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # print("You have acne.")
-        for detection in detections:
-            acne_class = detection['class']
-            confidence = detection['confidence']
-            # print(f"Type of acne: {acne_class}, Confidence: {confidence:.3f}")
+#         # Display the output image with bounding boxes
+#         image_result = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         # print("You have acne.")
+#         for detection in detections:
+#             acne_class = detection['class']
+#             confidence = detection['confidence']
+#             # print(f"Type of acne: {acne_class}, Confidence: {confidence:.3f}")
 
-        return image_result, acne_class, confidence
-    # tambahkan request post ke backend dimas
-    else:
-        return "congrats you dont have acne!!!"
+#         return image_result, acne_class, confidence
+#     # tambahkan request post ke backend dimas
+#     else:
+#         return "congrats you dont have acne!!!"
+    
+# def upload_to_gcs():
+
+# def post_request():
