74d61fe19a33ba9f08e1d38597b69d3ed1e0b9ab
tridims <tri.dimas@student.ub.ac.id>
2023-06-13 19:32:16 +0700

    reformat code with black and adding .vscode to gitignore

--- Analysis.py
+++ Analysis.py
@@ -4,8 +4,10 @@ import numpy as np
 import requests
 import urllib.request
 from keras.applications.mobilenet_v2 import preprocess_input
+
 # from keras.models import load_model # for testing purposes
 from google.cloud import storage
+
 
 def get_bounding_box(image, threshold):
     # Placeholder implementation
@@ -17,10 +19,11 @@ def get_bounding_box(image, threshold):
     ymax = int(height * 0.75)
     return xmin, ymin, xmax, ymax
 
+
 def detect_acne(data, model, threshold):
-    file_path = data['name']
-    file_name = file_path.split('/')[1]
-    bucket_name = data['bucket']
+    file_path = data["name"]
+    file_name = file_path.split("/")[1]
+    bucket_name = data["bucket"]
 
     image_path = f"https://storage.googleapis.com/{bucket_name}/{file_path}"
 
@@ -28,7 +31,7 @@ def detect_acne(data, model, threshold):
         # Read the input image using OpenCV
         req = urllib.request.urlopen(image_path)
         arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-        image = cv2.imdecode(arr, -1) # 'Load it as it is'
+        image = cv2.imdecode(arr, -1)  # 'Load it as it is'
     except Exception as e:
         print(f"DEBUG: exception when trying to read image. Error message: {e}")
         raise Exception("Error when trying to read image")
@@ -36,9 +39,9 @@ def detect_acne(data, model, threshold):
     try:
         # Resize the image to the required dimensions
         if image is None:
-            return ('DEBUG! Wrong path:', image_path)
+            return ("DEBUG! Wrong path:", image_path)
         else:
-            image = cv2.resize(image, dsize=(224,224))
+            image = cv2.resize(image, dsize=(224, 224))
 
         # Preprocess the image for model prediction
         input_data = preprocess_input(np.expand_dims(image, axis=0))
@@ -55,23 +58,15 @@ def detect_acne(data, model, threshold):
 
     # Extract the acne instances and their class labels
     detections = []
-    acne_labels = {
-        0: 'papules',
-        1: 'nodules',
-        2: 'pustules',
-        3: 'comedones'
-    }
+    acne_labels = {0: "papules", 1: "nodules", 2: "pustules", 3: "comedones"}
 
     for i, prediction in enumerate(predictions[0]):
         if prediction > threshold:
             acne_class = acne_labels[i]
             confidence = float(prediction)
 
-            detections.append({
-                'class': acne_class,
-                'confidence': confidence
-            })
-    
+            detections.append({"class": acne_class, "confidence": confidence})
+
     try:
         # Draw bounding boxes on the image for detected acne instances
         image_result = None
@@ -79,8 +74,8 @@ def detect_acne(data, model, threshold):
         confidence = None
         if len(detections) > 0:
             for detection in detections:
-                acne_class = detection['class']
-                confidence = detection['confidence']
+                acne_class = detection["class"]
+                confidence = detection["confidence"]
 
                 # Get the predicted bounding box coordinates
                 xmin, ymin, xmax, ymax = get_bounding_box(image, threshold)
@@ -93,8 +88,8 @@ def detect_acne(data, model, threshold):
             image_result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
             # print("You have acne.")
             for detection in detections:
-                acne_class = detection['class']
-                confidence = detection['confidence']
+                acne_class = detection["class"]
+                confidence = detection["confidence"]
         else:
             image_result = image
             acne_class = None
@@ -102,9 +97,14 @@ def detect_acne(data, model, threshold):
     except Exception as e:
         print(f"DEBUG: exception when trying to draw bounding box. Error message: {e}")
         raise Exception("Error when trying to draw bounding box")
-    
+
     try:
-        post_response = post_request(file_name=file_name, confidence=confidence, acne_class=acne_class, file_path=image_path)
+        post_response = post_request(
+            file_name=file_name,
+            confidence=confidence,
+            acne_class=acne_class,
+            file_path=image_path,
+        )
     except Exception as e:
         print(f"DEBUG: exception when trying to post request. Error message: {e}")
         raise Exception("Error when trying to post request")
@@ -116,30 +116,32 @@ def detect_acne(data, model, threshold):
 
     return upload_response, post_response
 
+
 def upload_to_gcs(bucket_name, image_result, file_name):
     # gcs_bucket_name = 'public-picture-media-bucket'
-    image_result = cv2.imwrite(f'static/{file_name}', image_result)
-    destination_blob_name = f'images_result/{file_name}'
-    storage_client = storage.Client(os.getenv('PROJECT_ID'))
+    image_result = cv2.imwrite(f"static/{file_name}", image_result)
+    destination_blob_name = f"images_result/{file_name}"
+    storage_client = storage.Client(os.getenv("PROJECT_ID"))
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
-    response = blob.upload_from_filename(f'static/{file_name}')
-    os.remove(f'static/{file_name}')
+    response = blob.upload_from_filename(f"static/{file_name}")
+    os.remove(f"static/{file_name}")
     return response
+
 
 def post_request(file_name, confidence, acne_class, file_path):
     gcs_bucket_url = f"{os.getenv('BUCKET_URL')}/images_result"
     data = {
-      "id": file_path,
-      "data": {
-          "confidence": confidence, 
-          "detectnedAcne": acne_class
-          },
-      "image": f"{gcs_bucket_url}/{file_name}",
-      "model": "acne"
+        "id": file_path,
+        "data": {"confidence": confidence, "detectnedAcne": acne_class},
+        "image": f"{gcs_bucket_url}/{file_name}",
+        "model": "acne",
     }
 
-    response = requests.post('https://skincheckai-api-b6zefxgbfa-et.a.run.app/machine-learning/report-analyses', data=data)
+    response = requests.post(
+        "https://skincheckai-api-b6zefxgbfa-et.a.run.app/machine-learning/report-analyses",
+        data=data,
+    )
 
     return response
 
